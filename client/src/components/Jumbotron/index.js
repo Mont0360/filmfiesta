36e3d2f778fd474926ce8409f3dd80b4cbaca0c6
Pangsua Vang <pangsua26@yahoo.com>
2020-08-25 21:37:15 -0500

    updated Jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,1 +1,14 @@
-export { default } from "./Jumbotron";
+import React from "react";
+
+function Jumbotron({ children }) {
+  return (
+    <div
+      style={{ height: 200, clear: "both", paddingTop: 80, textAlign: "center" }}
+      className="jumbotron"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
